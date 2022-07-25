c607d786160504c53a72b1756da93a02d2d01ed4
GBlachly <gblachly4@gmail.com>
2022-07-25 16:07:46 -0400

    Turned global arrays into an object

--- mixed-mess.js
+++ mixed-mess.js
@@ -1,7 +1,9 @@
 //Global Arrays
-let array1 = ['Bad', 'Terrible', 'Mean', 'Good', 'Great', 'Excellent'];
-let array2 = ['Dirty', 'Smelly', 'Ugly', 'Beatuiful', 'Attractive', 'Kind'];
-let array3 = ['Idiot', 'Dumby', 'Joker', 'Queen', 'King', 'Hero'];
+let messageObj = {
+    array1: ['Bad', 'Terrible', 'Mean', 'Good', 'Great', 'Excellent'],
+    array2: ['Dirty', 'Smelly', 'Ugly', 'Beatuiful', 'Attractive', 'Kind'],
+    array3: ['Idiot', 'Dumby', 'Joker', 'Queen', 'King', 'Hero']
+}
 
 //Randomizer function
 const random1 = (arr) => {
@@ -12,9 +14,9 @@ const random1 = (arr) => {
 
 //Message Randomizer function
 const mixedMessages = () => {
-    let a = random1(array1);
-    let b = random1(array2);
-    let c = random1(array3);
+    let a = random1(messageObj.array1);
+    let b = random1(messageObj.array2);
+    let c = random1(messageObj.array3);
     if (a[0] === 'A' || a[0] === 'E' || a[0] === 'I' || a[0] === 'O' || a[0] === 'U') {
         console.log(`You are an ${a.toLowerCase()} ${b.toLowerCase()} ${c.toLowerCase()}!`);
     } else {console.log(`You are a ${a.toLowerCase()} ${b.toLowerCase()} ${c.toLowerCase()}!`)};
